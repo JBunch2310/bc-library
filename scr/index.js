345edf6dbeb9e9b907a607f44d0966a866741282
Jamie Bunch <JamieBunch@ymail.com>
2022-01-31 15:18:25 -0500

    Update

--- scr/index.js
+++ scr/index.js
@@ -1,22 +1,22 @@
-const { response } = require('express');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
     response.send('Hello World');
 })
 
-app.post('/', (req, res) => {
+app.post('/users', (req, res) => {
     const {name, age, email} = req.body;
 
     const user = {name, age, email};
 
-    const result = 'My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}';
+    const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}.`;
 
     res.send(result);
 })
 
-app.listen(3000, () => {
-    console.log('we be listening on 3000');
+app.listen(3001, () => {
+    console.log('we be listening on 3001');
 })
